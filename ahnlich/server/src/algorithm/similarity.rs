b1f77b67f681ad6b5bc9041c4ce6848701be9981
David Onuh <davidonuh1@gmail.com>
2024-05-11 01:51:56 +0100

    add documentation for similarity functions

--- ahnlich/server/src/algorithm/similarity.rs
+++ ahnlich/server/src/algorithm/similarity.rs
@@ -29,6 +29,45 @@ impl From<&Algorithm> for SimilarityFunc {
     }
 }
 
+///
+/// ## COSINE SIMILARITY
+/// Cosine similiarity is the cosine of the angles between vectors.
+/// It tries to find how close or similar two vector points are.
+/// It is scalent invarient meaning it is unaffected by the length of the vectors.
+///
+///
+/// Cosine of the angles between vectors shows how similar, dissimilar or orthogonal(independent).
+///
+///
+/// The range of similarity for cosine similarity ranges from -1 to 1:
+/// where:
+///    -  1 means similar
+///    -  -1 different.
+///    -  0 means Orthogonal
+///
+///
+/// To calculate the cosine similarity for two vectors, we need to:
+///   - Calculate the dot product of both vectors
+///   - Find the product of the magnitude of both vectors.
+///          A magnitude of a vector can be calculated using pythagoras theorem:
+///          sqrt( A^2 + B^2)
+///             where A and B are two vectors.
+///
+///   - divide the dot product by the product of the magnitude of both vectors.
+///        ```
+///           cos(0) = A.B / ||A||.||B||
+///        ```
+///
+///     where Magnitude(A or B):
+///     ||A|| = sqrt[(1)^2 + (2)^2]
+///     
+///
+///  An Implementation for most similar items would be a MaxHeap.
+///  We are looking the closest number to one meaning Max
+///  The smaller the distance between two points, denotes higher
+///                                  similarity
+///
+
 fn cosine_similarity(
     first: &ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
     second: &ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
@@ -36,16 +75,12 @@ fn cosine_similarity(
     // formular = dot product of vectors / product of the magnitude of the vectors
     // maginiture of a vector can be calcuated using pythagoras theorem.
     // sqrt of sum of vector values
-    // for magnitutude of A( ||A||) =[1,2]
-    // sqrt[(1)^2 + (2)^2]
     //
     //
 
     let dot_product = dot_product(&first, &second);
 
     // the magnitude can be calculated using the arr.norm method.
-    //
-
     let mag_first = &first
         .iter()
         .map(|x| x * x)
@@ -64,27 +99,41 @@ fn cosine_similarity(
 
     cosine_sim
 }
-// for mulitplication of a matrix, the number of columns in the first matrix == the number of rows
-// in the second
-// A.B != B.A
+
+///
+/// ## DOT PRODUCT
+/// The dot product or scalar product is an algebraic operation that takes two equal-length
+/// sequences of numbers (usually coordinate vectors), and returns a single number.
+///
+/// An Implementation for most similar items would be a MaxHeap.
+/// The larger the dot product between two vectors, the more similar
 
 fn dot_product(
     first: &ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
     second: &ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
 ) -> f64 {
-    // transpose if the shapes are equal that is
-    // check that for a A = m*n and B = p * q
-    // m = q and n = q
-    // else check that it's already transposed
-
-    if first.shape() != second.shape() {
-        panic!("Incorrect dimensions, both vectors must match");
-    }
-
     let dot_product = second.dot(&first.t());
-
     dot_product
 }
+
+///  
+///  ## Euclidean Distance
+///     - d(p,q)= sqrt { (p-q)^2 }
+///  Euclidean distance is the square root of the sum of squared differences between corresponding
+///  elements of the two vectors.
+///
+///  Note that the formula treats the values of P and Q seriously: no adjustment is made for
+///  differences in scale. Euclidean distance is only appropriate for data measured on the same
+///  scale(meaning it is  scale invarient).
+///  The distance derived is purely based on the difference between both vectors and is therefore,
+///  prone to skewness if the units of the vectors are vastly different.
+///
+///  Hence, it is important to ensure that the data is normalised before applying the Euclidean
+///  distance function.
+///
+///  An Implementation for most similar items would be a MinHeap, The smaller the distance between
+///  two points, denotes higher similarity
+///
 
 fn euclidean_distance(
     first: &ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
