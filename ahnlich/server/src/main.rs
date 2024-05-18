376ebd961925024bd4c584952be0552ef7b8b9de
David Onuh <davidonuh1@gmail.com>
2024-05-18 10:23:56 +0100

    specify server run args

--- ahnlich/server/src/main.rs
+++ ahnlich/server/src/main.rs
@@ -7,6 +7,7 @@ mod network;
 mod storage;
 use std::error::Error;
 
+use clap::{ArgAction, Args, Parser, Subcommand};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -35,9 +36,52 @@ async fn process_stream(stream: TcpStream) -> Result<(), tokio::io::Error> {
     }
 }
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Args, Debug)]
+struct Run {
+    #[arg(long, default_value_t = String::from("127.0.0.1"))]
+    host: String,
+
+    #[arg(long, default_value_t = 1369)]
+    port: u16,
+
+    /// Allows server to persist data to disk on occassion
+    #[arg(long, default_value_t = false, action=ArgAction::SetTrue)]
+    enable_persistence: bool,
+
+    /// persistence location
+    #[arg(long, requires_if("true", "enable_persistence"))]
+    persist_location: Option<std::path::PathBuf>,
+
+    /// persistence intervals in milliseconds
+    #[arg(long, default_value_t = 1000 * 60 * 5)]
+    persistence_intervals: u64,
+}
+
+#[derive(Args, Debug)]
+struct Persistence {}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Starts Anhlich server
+    Run(Run),
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    run_server().await?;
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Run(run) => {
+            println!("{:?}", run);
+            run_server().await?;
+        }
+    }
     Ok(())
 }
 
