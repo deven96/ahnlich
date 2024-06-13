f0b1d8c55f4fd842be59b2f78d0fe88e07da1fe8
David Onuh <davidonuh1@gmail.com>
2024-06-13 15:17:53 +0100

    Refactor protocol and fix poetry dir issue

---
+++ sdk/ahnlich-client-py/README.md
@@ -0,0 +1,3 @@
+## Ahnlich Client PY
+
+A Python client that interacts with both ahnlich DB and AI
--- sdk/ahnlich-client-py/protocol.py
+++ sdk/ahnlich-client-py/protocol.py
@@ -1,33 +1,56 @@
-import typing
-
 import socket
-import serde_types as st
+
 import query
 import server_response
 
 HEADER = b"AHNLICH;"
 BUFFER_SIZE = 1024
 
-class BincodeDeAndSerializer:
 
-    def __init__(self):
-        super().__init__()
-
+class AhnlichProtocol:
+    def __init__(self, address: str, port: int):
+        self.address = address
+        self.port = port
+        self.client = self.connect()
 
     def serialize_query(self, server_query: query.ServerQuery) -> bytes:
-
-        version = server_response.Version(0,1,0).bincode_serialize()
-        response =  server_query.bincode_serialize()
+        version = server_response.Version(0, 1, 0).bincode_serialize()
+        response = server_query.bincode_serialize()
         response_length = int(len(response)).to_bytes(8, "little")
         return HEADER + version + response_length + response
 
     def deserialize_server_response(self, b: bytes) -> server_response.ServerResult:
         return server_response.ServerResult([]).bincode_deserialize(b)
 
+    def connect(self) -> socket.socket:
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket.connect((self.address, self.port))
+        return tcp_socket
+
+    def send(self, message: query.ServerQuery):
+        serialized_bin = self.serialize_query(message)
+        self.client.sendall(serialized_bin)
+
+    def receive(self):
+        header = self.client.recv(8)
+        if header != HEADER:
+            exit("Fake server")
+        # ignore version of 5 bytes
+        version = self.client.recv(5)
+        print(server_response.Version.bincode_deserialize(version))
+        length = self.client.recv(8)
+        # header length u64, little endian
+        length_to_read = int.from_bytes(length, byteorder="little")
+        # information data
+        self.client.settimeout(5)
+        data = self.client.recv(length_to_read)
+        response = self.deserialize_server_response(data)
+        return response
+
 
 def test_serialize():
     ping = query.ServerQuery(
-        queries = [
+        queries=[
             query.Query__Ping(),
             query.Query__InfoServer(),
             query.Query__ListClients(),
@@ -39,21 +62,8 @@ def test_serialize():
             ),
         ],
     )
-    message = BincodeDeAndSerializer().serialize_query(ping)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(("127.0.0.1", 1369))
-        s.sendall(message)
-        header = s.recv(8)
-        if header != HEADER:
-            exit("Fake server")
-        # ignore version of 5 bytes
-        version = s.recv(5)
-        print(server_response.Version.bincode_deserialize(version))
-        length = s.recv(8)
-        # header length u64, little endian
-        length_to_read = int.from_bytes(length, byteorder="little")
-        # information data
-        s.settimeout(5)
-        data = s.recv(length_to_read)
-        print(BincodeDeAndSerializer().deserialize_server_response(data))
+    ahnlich_protocol = AhnlichProtocol(address="127.0.0.1", port=1369)
+    ahnlich_protocol.send(message=ping)
+    response = ahnlich_protocol.receive()
+    print(response)
 
