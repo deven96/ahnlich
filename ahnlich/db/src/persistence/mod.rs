35b4dcce5f36c5ddfec166e550cde7105ad881fe
Diretnan Domnan <diretnandomnan@gmail.com>
2024-06-14 12:07:39 +0200

    Write to tempfile and replace persist_location after

--- ahnlich/db/src/persistence/mod.rs
+++ ahnlich/db/src/persistence/mod.rs
@@ -1,9 +1,10 @@
 use crate::engine::store::Stores;
 use std::fs::File;
-use std::fs::OpenOptions;
+use std::path::Path;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
+use tempfile::NamedTempFile;
 use tokio::select;
 use tokio::time::sleep;
 use tokio::time::Duration;
@@ -47,19 +48,24 @@ impl PersistenceTask {
                 }
                 has_potential_write = self.has_potential_write() => {
                     if has_potential_write {
-                        let writer = if let Ok(file) = OpenOptions::new().create(true).truncate(true).write(true).open(&self.persist_location) {
+                        let persist_location: &Path = self.persist_location.as_ref();
+                        let writer = if let Ok(file) = NamedTempFile::new_in(persist_location.parent().expect("Could not get parent directory of persist location")) {
                             file
                         } else {
                             tracing::error!("Could not create persistence file, skipping");
                             continue;
                         };
+                        let temp_path = writer.path();
                         // set write flag to false before writing to it
                         let _ = self.write_flag.compare_exchange(true, false, Ordering::SeqCst, Ordering::SeqCst);
-                        if let Err(e) = serde_json::to_writer(writer, &self.stores) {
-                            tracing::error!("Error persisting stores {e}");
+                        if let Err(e) = serde_json::to_writer(&writer, &self.stores) {
+                            tracing::error!("Error writing stores to temp file {e}");
 
                         } else {
-                            tracing::debug!("Persisted stores to disk");
+                            match std::fs::rename(temp_path, persist_location) {
+                                Ok(_) => tracing::debug!("Persisted stores to disk"),
+                                Err(e) => tracing::error!("Error writing temp file to persist location {e}"),
+                            };
                         }
                     } else {
                         tracing::debug!("No potential writes happened during persistence interval")
