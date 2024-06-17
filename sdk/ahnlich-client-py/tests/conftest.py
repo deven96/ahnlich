7e18b18a5ee5fa861846406953ea8e1fa98c66f3
David Onuh <davidonuh1@gmail.com>
2024-06-17 10:06:11 +0100

    debug: print out host and port from env

--- sdk/ahnlich-client-py/tests/conftest.py
+++ sdk/ahnlich-client-py/tests/conftest.py
@@ -3,10 +3,9 @@ from internals import protocol
 import os
 
 
-
-
 @pytest.fixture
 def base_protocol():
     host = os.environ.get("AHNLICH_DB_HOST", "127.0.0.1")
     port = int(os.environ.get("AHNLICH_DB_PORT", 1369))
+    print(host, port)
     return protocol.AhnlichProtocol(address=host, port=port)
