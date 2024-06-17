838ecf05cd5b8c0fb5a34cb9275c0e4ff49d798d
David Onuh <davidonuh1@gmail.com>
2024-06-17 01:20:03 +0100

    setting up job to test python client

--- sdk/ahnlich-client-py/tests/conftest.py
+++ sdk/ahnlich-client-py/tests/conftest.py
@@ -1,7 +1,12 @@
 import pytest
 from internals import protocol
+import os
+
+
 
 
 @pytest.fixture
 def base_protocol():
-    return protocol.AhnlichProtocol(address="127.0.0.1", port=1369)
+    host = os.environ.get("AHNLICH_DB_HOST", "127.0.0.1")
+    port = int(os.environ.get("AHNLICH_DB_PORT", 1369))
+    return protocol.AhnlichProtocol(address=host, port=port)
