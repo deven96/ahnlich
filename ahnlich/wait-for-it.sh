289561bed3beb2e6f4a2fe52589ac67811ebec68
David Onuh <davidonuh1@gmail.com>
2024-06-17 11:46:02 +0100

    parse out port in nc commands

--- ahnlich/wait-for-it.sh
+++ ahnlich/wait-for-it.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 # Use this script to wait for a service to be ready
 
-host="$1"
+host=$1
+shift
+port=$1
 shift
 cmd="$@"
 
-until nc -z -v -w30 $host; do
-  >&2 echo "Waiting for $host to be available..."
+until nc -z -v -w30 $host $port; do
+  >&2 echo "Waiting for $host:$port to be available..."
   sleep 1
 done
 
->&2 echo "$host is available"
+>&2 echo "$host:$port is available"
 exec $cmd
