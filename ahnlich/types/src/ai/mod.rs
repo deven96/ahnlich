e83534c35e9e219c7bbe16f8c92e9c8e290ba56a
David Onuh <davidonuh1@gmail.com>
2024-06-29 22:37:26 +0100

    Create ai-proxy query enum

--- ahnlich/types/src/ai/mod.rs
+++ ahnlich/types/src/ai/mod.rs
@@ -1,2 +1,10 @@
 mod query;
 mod server;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum AIModel {}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum AIStoreType {}
--- ahnlich/types/src/ai/query.rs
+++ ahnlich/types/src/ai/query.rs
@@ -1,1 +1,53 @@
+use super::{AIModel, AIStoreType};
+use crate::keyval::{StoreKey, StoreName, StoreValue};
+use crate::metadata::{MetadataKey, MetadataValue};
+use crate::predicate::PredicateCondition;
+use crate::similarity::Algorithm;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+use std::num::NonZeroUsize;
 
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum AiQuery {
+    CreateStore {
+        r#type: AIStoreType,
+        store: StoreName,
+        model: AIModel,
+        predicates: HashSet<MetadataKey>,
+    },
+    GetPred {
+        store: StoreName,
+        condition: PredicateCondition,
+    },
+    GetSimN {
+        store: StoreName,
+        search_input: MetadataValue,
+        condition: Option<PredicateCondition>,
+        closest_n: NonZeroUsize,
+        algorithm: Algorithm,
+    },
+    CreateIndex {
+        store: StoreName,
+        predicates: HashSet<MetadataKey>,
+    },
+    DropIndexPred {
+        store: StoreName,
+        predicates: HashSet<MetadataKey>,
+        error_if_not_exists: bool,
+    },
+    Set {
+        store: StoreName,
+        inputs: Vec<MetadataValue>,
+    },
+    DelKey {
+        store: StoreName,
+        key: MetadataValue,
+    },
+    DropStore {
+        store: StoreName,
+        error_if_not_exists: bool,
+    },
+    InfoServer,
+    ListStores,
+    Ping,
+}
