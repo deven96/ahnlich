b4b9be80dda9c7c3cffec25920ec137cd496d993
Diretnan Domnan <diretnandomnan@gmail.com>
2024-09-23 20:06:03 +0200

    WIP: Using chunking in predicate add

---
+++ ahnlich/utils/src/parallel.rs
@@ -0,0 +1,22 @@
+use rayon::ThreadPoolBuilder;
+use std::sync::Once;
+
+static INIT_THREADPOOL_ONCE: Once = Once::new();
+
+// Initialize global rayon threadpool
+pub(crate) fn init_threadpool(num_threads: usize) {
+    INIT_THREADPOOL_ONCE.call_once(|| {
+        ThreadPoolBuilder::new()
+            .num_threads(num_threads)
+            .build_global()
+            .expect("Cannot build server threadpool");
+    });
+}
+
+// Calculates chunk size to use for an iterable input in order for it to be able to fit into all
+// possible rayon threads
+pub fn chunk_size(input_length: usize) -> usize {
+    let num_threads = rayon::current_num_threads();
+    let minimum_factor = std::cmp::min(input_length, num_threads);
+    (input_length + minimum_factor - 1) / minimum_factor
+}
