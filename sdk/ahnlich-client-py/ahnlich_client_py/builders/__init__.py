063c7d836a319eddb0cadaf45549839f8a0f4a08
David Onuh <davidonuh1@gmail.com>
2024-11-19 18:09:47 +0100

    WIP: Async clients: Added async db client and builder with accompanying tests

--- sdk/ahnlich-client-py/ahnlich_client_py/builders/__init__.py
+++ sdk/ahnlich-client-py/ahnlich_client_py/builders/__init__.py
@@ -1,7 +1,9 @@
 from .ai import AhnlichAIRequestBuilder
 from .db import AhnlichDBRequestBuilder
+from .non_blocking import AsyncAhnlichDBRequestBuilder
 
 __all__ = [
     "AhnlichDBRequestBuilder",
     "AhnlichAIRequestBuilder",
+    "AsyncAhnlichDBRequestBuilder"
 ]
