bee14b7ea646782a8b5f782d2fb6db70dc8c16fa
David Onuh <davidonuh1@gmail.com>
2025-02-10 00:04:38 +0100

    Ahnlich Protocol Swap Spec (#195)
    * WIP: create Grpc spec for ahnlich protocol swap
    
    * Generate proto files for ai and db query,response. Build file now generates rust types into src crate
    
    * add generated files from protos. Build script automatically checks for changes in proto file
    
    * added ai and db service. Ran cargo fmt
    
    * setup protobuf in github actions
    
    * clippy lints in grpc build file
    
    * change NOTE to TODO in build.rs
    
    * use tonic to build server and client for ai and db
    
    * update build.rs
    
    * change grpc spec names for fix clippy errors
    
    * add utils mod and exclude from build.rs
    
    * add simple readme for protofiles
    
    * add comments to messages and update specs
    
    ---------
    
    Co-authored-by: Diretnan Domnan <diretnandomnan@gmail.com>

---
+++ ahnlich/grpc_types/src/ai/mod.rs
@@ -0,0 +1,6 @@
+pub mod execution_provider;
+pub mod models;
+pub mod pipeline;
+pub mod preprocess;
+pub mod query;
+pub mod server;
---
+++ ahnlich/grpc_types/src/db/mod.rs
@@ -0,0 +1,3 @@
+pub mod pipeline;
+pub mod query;
+pub mod server;
