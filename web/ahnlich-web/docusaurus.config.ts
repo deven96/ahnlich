b566429549df12932b69cd03110fc665b117d772
DeeDee <43624344+SLKhadeeja@users.noreply.github.com>
2025-07-17 22:20:29 +0100

    updated default url (#235)

--- web/ahnlich-web/docusaurus.config.ts
+++ web/ahnlich-web/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/logo.jpg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ahnlich.tech',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
