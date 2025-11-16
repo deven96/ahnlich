3452483a9be3f348332346a5cd2483f37c7f2c4b
Diretnan Domnan <diretnandomnan@gmail.com>
2025-11-16 14:23:09 +0100

    Switching ahnlich.tech to ahnlich.dev

--- web/ahnlich-web/docusaurus.config.ts
+++ web/ahnlich-web/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
     themes: ['@docusaurus/theme-mermaid'],
 
   // Set the production url of your site here
-  url: 'https://ahnlich.tech',
+  url: 'https://ahnlich.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
