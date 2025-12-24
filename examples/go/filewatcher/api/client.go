7ee450dcd72806695387bee6f3b42bf4069e6611
chijioke <michaelsylva36@gmail.com>
2025-12-24 06:09:57 +0100

    feat(watcher): added debounce, logger and client

--- examples/go/filewatcher/api/client.go
+++ examples/go/filewatcher/api/client.go
@@ -1,1 +1,39 @@
 package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"go/ahnlich/events"
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	url    string
+	client *http.Client
+}
+
+func New(apiURL string) *Client {
+	return &Client{
+		url: apiURL,
+		client: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func (c *Client) SendEvent(event events.FileEvent) error {
+	body, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, c.url, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	_, err = c.client.Do(req)
+	return err
+}
