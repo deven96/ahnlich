d34979e55bc84f1457e69dea15d9acd5d3c253fa
Jim Ezesinachi <ezesinachijim@gmail.com>
2026-05-18 09:50:18 +0100

    added state machine lock check error
    Signed-off-by: Jim Ezesinachi <ezesinachijim@gmail.com>

--- ahnlich/replication/src/storage/memory.rs
+++ ahnlich/replication/src/storage/memory.rs
@@ -44,6 +44,16 @@ impl<C: RaftTypeConfig> MemLogStore<C>
 where
     C::Entry: RaftLogId<C::NodeId> + Clone,
 {
+    fn lock_inner(
+        &self,
+    ) -> Result<std::sync::MutexGuard<'_, MemLogStoreInner<C>>, StorageError<C::NodeId>> {
+        self.inner.lock().map_err(|_| StorageError::IO {
+            source: openraft::StorageIOError::read_logs(&std::io::Error::other(
+                "mem log lock poisoned",
+            )),
+        })
+    }
+
     fn last_log_id(inner: &MemLogStoreInner<C>) -> Option<LogIdOf<C>> {
         inner
             .log
@@ -62,7 +72,7 @@ where
         &mut self,
         range: RB,
     ) -> Result<Vec<C::Entry>, StorageError<C::NodeId>> {
-        let inner = self.inner.lock().expect("mem log lock poisoned");
+        let inner = self.lock_inner()?;
         let mut out = Vec::new();
         for (_, ent) in inner.log.range(range) {
             out.push(ent.clone());
@@ -76,7 +86,7 @@ where
     C::Entry: RaftLogId<C::NodeId> + Clone,
 {
     fn get_log_state_sync(&mut self) -> Result<LogState<C>, StorageError<C::NodeId>> {
-        let inner = self.inner.lock().expect("mem log lock poisoned");
+        let inner = self.lock_inner()?;
         Ok(LogState {
             last_purged_log_id: inner.last_purged_log_id.clone(),
             last_log_id: Self::last_log_id(&inner),
@@ -84,13 +94,13 @@ where
     }
 
     fn save_vote_sync(&mut self, vote: &Vote<C::NodeId>) -> Result<(), StorageError<C::NodeId>> {
-        let mut inner = self.inner.lock().expect("mem log lock poisoned");
+        let mut inner = self.lock_inner()?;
         inner.vote = Some(vote.clone());
         Ok(())
     }
 
     fn read_vote_sync(&mut self) -> Result<Option<Vote<C::NodeId>>, StorageError<C::NodeId>> {
-        let inner = self.inner.lock().expect("mem log lock poisoned");
+        let inner = self.lock_inner()?;
         Ok(inner.vote.clone())
     }
 
@@ -98,7 +108,7 @@ where
     where
         I: IntoIterator<Item = C::Entry> + OptionalSend,
     {
-        let mut inner = self.inner.lock().expect("mem log lock poisoned");
+        let mut inner = self.lock_inner()?;
         for entry in entries {
             inner.log.insert(entry.get_log_id().index, entry);
         }
@@ -109,7 +119,7 @@ where
         &mut self,
         log_id: openraft::LogId<C::NodeId>,
     ) -> Result<(), StorageError<C::NodeId>> {
-        let mut inner = self.inner.lock().expect("mem log lock poisoned");
+        let mut inner = self.lock_inner()?;
         let keys: Vec<u64> = inner.log.range(log_id.index..).map(|(k, _)| *k).collect();
         for k in keys {
             inner.log.remove(&k);
@@ -121,7 +131,7 @@ where
         &mut self,
         log_id: openraft::LogId<C::NodeId>,
     ) -> Result<(), StorageError<C::NodeId>> {
-        let mut inner = self.inner.lock().expect("mem log lock poisoned");
+        let mut inner = self.lock_inner()?;
         let keys: Vec<u64> = inner.log.range(..=log_id.index).map(|(k, _)| *k).collect();
         for k in keys {
             inner.log.remove(&k);
@@ -134,7 +144,7 @@ where
         &mut self,
         committed: Option<openraft::LogId<C::NodeId>>,
     ) -> Result<(), StorageError<C::NodeId>> {
-        let mut inner = self.inner.lock().expect("mem log lock poisoned");
+        let mut inner = self.lock_inner()?;
         inner.committed = committed;
         Ok(())
     }
@@ -142,7 +152,7 @@ where
     fn read_committed_sync(
         &mut self,
     ) -> Result<Option<openraft::LogId<C::NodeId>>, StorageError<C::NodeId>> {
-        let inner = self.inner.lock().expect("mem log lock poisoned");
+        let inner = self.lock_inner()?;
         Ok(inner.committed.clone())
     }
 }
